4251c8d8ffde5d7dd3a5d6f8672775156535d931
Txaverria <isaac.chc24@gmail.com>
2024-07-03 10:41:34 -0600

    Favicon, header, scrolltop y audio

---
+++ Tarea3/js/script.js
@@ -0,0 +1,13 @@
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollTopBtn.classList.add("show");
+    } else {
+        scrollTopBtn.classList.remove("show");
+    }
+};
+
+scrollTopBtn.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
